e9b1f045ae6e3daee9668775b2e55f32c56eb343
Dimitris Zervas <dzervas@dzervas.gr>
2015-05-01 04:13:34 +0300

    Added apple

--- snake.py
+++ snake.py
@@ -1,46 +1,49 @@
 from curtsies import input
 from curtsies.window import FullscreenWindow
 from curtsies.formatstringarray import FSArray
-#from curtsies.termhelpers import Cbreak, Nonblocking
+from curtsies.formatstring import fmtstr
+from random import randrange
 from time import time
 import sys
 
 # Body and head ascii form of the snake and apple.
 	# head, body, apple, space
 theme = [ ' ', 'o', '0', '@' ]
-sspeed = 1
-acc = 0.25
-accevery = 5
+sspeed = 3
+acc = 1
 
 # Moves the snake one step
 # hcoor: Coordinates of the snake's head. [row, column]
 # hcoor: Coordinates of the snake's last bit of tail. [row, column]
-def step(current, hcoor, tcoor, direction, length):
-	global theme, rows, columns
+def step():
+	global theme, screen, hcoor, tcoor, moves, length
 
-	result = current
+	oldcoor = hcoor
+	result = screen
 	# Override body (previous head)
 	result[hcoor[0], hcoor[1]] = theme[1]
 	# Remove old tail
 	result[tcoor[0], tcoor[1]] = theme[0]
 
 	# Place head
-	if direction[-1] == "up":
-		result[hcoor[0] - 1, hcoor[1]] = theme[2]
+	if moves[-1] == "up":
 		hcoor[0] -= 1
-	elif direction[-1] == "down":
-		result[hcoor[0] + 1, hcoor[1]] = theme[2]
+	elif moves[-1] == "down":
 		hcoor[0] += 1
-	elif direction[-1] == "right":
-		result[hcoor[0], hcoor[1] + 1] = theme[2]
+	elif moves[-1] == "right":
 		hcoor[1] += 1
-	elif direction[-1] == "left":
-		result[hcoor[0], hcoor[1] - 1] = theme[2]
+	elif moves[-1] == "left":
 		hcoor[1] -= 1
 	else:
-		return current
+		return
 
-	tdir = direction.pop(0)
+	if result[hcoor[0], hcoor[1]][0] == theme[3]:
+		apple()
+		tdir = None
+	else:
+		tdir = moves.pop(0)
+
+	result[hcoor[0], hcoor[1]] = theme[2]
 
 	if tdir == "up":
 		tcoor[0] -= 1
@@ -50,40 +53,55 @@ def step(current, hcoor, tcoor, direction, length):
 		tcoor[1] += 1
 	elif tdir == "left":
 		tcoor[1] -= 1
-	else:
-		return current
 
-	return result
+	screen = result
+
+def apple():
+	global theme, screen, points, length, rows, columns, speed, acc
+
+	points += 1
+	length += 1
+	speed += acc
+
+	coords = [randrange(0, rows), randrange(0, columns)]
+
+	while screen[coords[0], coords[1]] == theme[0]:
+		coords = [randrange(0, rows), randrange(0, columns)]
+
+	screen[coords[0], coords[1]] = theme[3]
 
 with FullscreenWindow(sys.stdout) as w:
 	with input.Input(sys.stdin) as input_generator:
 		rows, columns = w.t.height, w.t.width
-		speed = sspeed
-		stage = last = time()
 		hcoor = [rows / 2 - 3, columns / 2]
 		tcoor = [rows / 2, columns / 2]
+
 		direction = "up"
 		length = 3
+		points = 0
+
+		speed = sspeed
+		last = time()
 		moves = []
+
 		for _ in range(length):
 			moves.append(direction)
 
 		screen = FSArray(rows, columns)
 
+		coords = [randrange(0, rows), randrange(0, columns)]
+		while screen[coords[0], coords[1]] == theme[0]:
+			coords = [randrange(0, rows), randrange(0, columns)]
+		screen[coords[0], coords[1]] = theme[3]
+
 		while True:
-
-			if time() - stage >= accevery:
-				stage = time()
-				#speed += acc
-
+			print points
 			if time() - last >= 1.0 / speed:
-				print time() - last
 				last = time()
 
-				# TODO: Manage the length change
 				moves.append(direction)
 
-				screen = step(screen, hcoor, tcoor, moves, length)
+				step()
 
 			c = input_generator.next()
 #
