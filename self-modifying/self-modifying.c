e70e143e03c67ca9827a53bfc53c81909392508a
Dimitris Zervas <dzervas@dzervas.gr>
2017-04-12 04:04:14 +0300

    Fix self modifying code

--- self-modifying/self-modifying.c
+++ self-modifying/self-modifying.c
@@ -9,15 +9,15 @@
 from readelf -a <executable>
 f definition:
 00000000004005e6 <f>:
-	4005e6		55			push   %rbp
-	4005e7		48 89 e5		mov    %rsp,%rbp
+	4005e6		55					push   %rbp
+	4005e7		48 89 e5			mov    %rsp,%rbp
 	4005ea		be 01 00 00 00		mov    $0x1,%esi
 	4005ef		bf 04 07 40 00		mov    $0x400704,%edi
 	4005f4		b8 00 00 00 00		mov    $0x0,%eax
 	4005f9		e8 92 fe ff ff		callq  400490 <printf@plt>
-	4005fe		90			nop
-	4005ff		5d			pop    %rbp
-	400600		c3			retq
+	4005fe		90					nop
+	4005ff		5d					pop    %rbp
+	400600		c3					retq
 */
 void f() {
 	printf("%d\n", 1);
@@ -25,6 +25,7 @@ void f() {
 
 void g() {
 	printf("Hello World! :)\n");
+	printf("second line!\n");
 }
 
 void end() {}
@@ -41,11 +42,9 @@ int main() {
 	mprotect(page, getpagesize(), PROT_READ | PROT_WRITE | PROT_EXEC);
 	mprotect(page, getpagesize(), PROT_READ | PROT_WRITE | PROT_EXEC);
 
-	printf("%x\n", *p);
+	printf("%zx\n", *p);
 	f();
 	memcpy((f+10), *g, sizeof(unsigned long int));
-	f();
-	memcpy((void*) 0x000000000040083d, g, g - end);
 	f();
 
 	return 0;
